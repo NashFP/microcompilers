c8b6fbb339169aa7c368023c6d2d4034a094e5f3
Jason Orendorff <jason.orendorff@gmail.com>
2018-11-28 08:44:11 -0600

    My assembly-formatting code in Clojure.

---
+++ jorendorff+clojure/README.md
@@ -0,0 +1,20 @@
+# microcompilers
+
+My sketch of a compiler is in [assemble.clj](assemble.clj).
+
+I am calling my little language "micro".
+
+
+### TODO
+
+* Stand up an end-to-end test that runs hello.micro
+
+* Stand up some unit tests for instructions
+
+* Stop using keywords for registers just because they are there
+
+* Try describing this language (at least loosely) using a Clojure spec
+
+* Assembly language features that might be worth trying out:
+    * 4-operand indirect addressing mode (lol)
+    * indirect `jmp` and `call`
---
+++ jorendorff+clojure/assemble.clj
@@ -0,0 +1,84 @@
+(use '[clojure.string :only (join)])
+
+(def registers
+  '{:%rax :%rbx :%rcx :%rdx :%rbp :%rsp :%rsi :%rdi
+    :%r8 :%r9 :%r10 :%r11 :%r12 :%r13 :%r14 :%r15})
+
+(defn reg->str
+  "Convert an x86-64 register from its Clojure form (a keyword) to a string.
+  This strips off the `:` but leaves the `%`."
+  [reg] (subs (str reg) 1))
+
+(defn operand->str
+  "Convert an x86-64 instruction operand from its Clojure form to a string."
+  [arg]
+  (cond
+    (integer? arg) (str "$" arg)
+    (symbol? arg) (str arg)
+    (keyword? arg) (subs (str arg) 1)
+    (vector? arg) (if (and (= (count arg) 2)
+                           (or (integer? (first arg))
+                               (symbol? (first arg))))
+                    (str (first arg) "(" (operand->str (second arg)) ")")
+                    (str "(" (join ", " (map operand->str arg)) ")"))))
+
+(defn insn->str
+  "Convert one x86-64 instruction from its Clojure list form to a string,
+  one line of assembly."
+  [insn] (let [name (first insn)
+               args (rest insn)]
+           (str "\t" name "\t" (join ", " (map operand->str args)))))
+
+;; Assembly files are not just instructions.
+;; They also contain "directives" to tell the assembler what to do with the instructions.
+;; These functions emit directives.
+(def directive-formatters
+  {'.section #(join "," %&)
+   '.label #(str %1 ":")
+   '.asciz #(pr-str %1)
+   })
+
+(defn default-format [& args]
+  (join ", " (map str args)))
+
+(defn directive->str
+  "Convert an assembler directive from its Clojure list form to a string."
+  [source]
+  (let [command (first source)
+        args (rest source)]
+    (if (= command '.label)
+      (str (first args) ":")
+      (str "\t" command "\t"
+           (apply (directive-formatters command default-format) args)))))
+
+(defn split-comment
+  "We support comments by letting the user add `:# \"string\"` at the end of any list.
+  This function strips the comment, if any, from the given `source` list.
+  Returns a vector, [uncommented-line comment-or-nil]."
+  [commented-line]
+  (let [n (count commented-line)]
+    (if (and (>= n 2)
+             (= (nth commented-line (- n 2)) :#))
+      (vector (take (- n 2) commented-line) (last commented-line))
+      (vector commented-line nil))))
+
+(defn directive? [line]
+  (.startsWith (str (first line)) "."))
+
+(defn emit-comment [comment]
+  (if (nil? comment)
+    ""
+    (str "\t## " comment)))
+
+(defn line->str [commented-line]
+  (let [[line comment] (split-comment commented-line)
+        code-line (cond
+                    (nil? line) ""
+                    (directive? line) (directive->str line)
+                    :else (insn->str line))]
+    (str code-line (emit-comment comment) "\n")))
+
+(let [program (binding [*read-eval* false] (read))
+      lines (map line->str program)
+      output (join lines)]
+  (print output))
